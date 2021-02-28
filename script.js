a713edc67772f11739cca1cc37e29d9bc7472681
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-02-28 14:49:34 -0500

    Brought in the DOM elements into the javascript file.

---
+++ script.js
@@ -0,0 +1,10 @@
+// DOM Elements.
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreElement = document.getElementById("score");
+const timeElement = document.getElementById("time");
+const endGameElement = document.getElementById("end-game");
+const settingsButton = document.getElementById("settings-button");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
