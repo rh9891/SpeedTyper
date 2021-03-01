c82469745e6efd2bbdf943d3b71a63aeb876f948
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-01 15:17:34 -0500

    Created an array of words and created a function to generate a random word from the created array.

--- script.js
+++ script.js
@@ -8,3 +8,48 @@ const settingsButton = document.getElementById("settings-button");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
 const difficultySelect = document.getElementById("difficulty");
+
+// Array of words for game.
+const words = [
+  "fastness",
+  "swiftness",
+  "upper",
+  "accelerate",
+  "hasten",
+  "hie",
+  "hotfoot",
+  "race",
+  "rush",
+  "hurrying",
+  "speeding",
+  "hurry",
+  "zip",
+  "quicken",
+  "velocity",
+  "ready",
+  "speedy",
+  "immediate",
+  "prompt",
+  "straightaway",
+  "agile",
+  "nimble",
+  "spry",
+  "promptly",
+  "quickly",
+  "fast",
+  "flying",
+];
+
+// Initialize the word.
+let randomWord;
+
+// Initialize the score.
+let score = 0;
+
+// Initialize the time.
+let time = 10;
+
+// Function to generate a random word from the words array.
+getRandomWord = () => {
+  return words[Math.floor(Math.random() * words.length)];
+};
