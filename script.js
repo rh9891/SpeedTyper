c8c8c8ac7e97d985219e2185c57bd508834c2573
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-03-01 22:11:13 -0500

    On start game, the focus method is now called to go directly to input. Created function to count down time and display it on DOM. Created function to end game and display end screen on DOM. Changed CSS for button to display flex, display final score, and game message to user.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const word = document.getElementById("word");
 const text = document.getElementById("text");
 const scoreElement = document.getElementById("score");
 const timeElement = document.getElementById("time");
-const endGameElement = document.getElementById("end-game");
+const endGameElement = document.getElementById("end-game-container");
 const settingsButton = document.getElementById("settings-button");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
@@ -49,9 +49,39 @@ let score = 0;
 // Initialize the time.
 let time = 10;
 
+// On game start, calling focus method to focus on text.
+text.focus();
+
+// Function to start the timer to count down.
+const timeInterval = setInterval(updateTime, 1000);
+
 // Function to generate a random word from the words array.
 getRandomWord = () => {
   return words[Math.floor(Math.random() * words.length)];
+};
+
+// Function to update the timer and display it on the DOM.
+function updateTime() {
+  time--;
+  timeElement.innerHTML = time + "s";
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+
+    // Function to end game.
+    gameOver();
+  }
+}
+
+// Function to end game and display the end screen on DOM.
+gameOver = () => {
+  endGameElement.innerHTML = `
+<h1>Sorry! Time has run out.</h1>
+<p class="final-score-message">Your final score is ${score}.</p>
+<button onClick="window.location.reload()" class="button play-again-button">Play Again</button>
+`;
+
+  endGameElement.style.display = "flex";
 };
 
 // Function to display random word onto DOM.
