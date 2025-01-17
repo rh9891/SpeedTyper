f537db9080c7cdf6c89965f6d24027ab29c588af
rh9891 <romie.hecdivert@gmail.com>
2025-01-16 23:14:06 -0500

    Added functionality to record scores in localStorage. Added high score table. Modified styling.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Speed Typer
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+![JavaScript](https://img.shields.io/badge/JavaScript-ES6+-yellowgreen)
+![HTML](https://img.shields.io/badge/HTML-5-orange)
+![CSS](https://img.shields.io/badge/CSS-3-blue)
+![localStorage](https://img.shields.io/badge/Storage-localStorage-brightgreen)
+![API](https://img.shields.io/badge/API-Custom-blue)
 
 ## Table of Contents
 
@@ -11,15 +16,18 @@
 - [Links](#links)
 - [Built Using](#built-using)
 - [License](#license)
-- [Questions](#questions)
 
 ## Description
 
-Challenge yourself as you race against the clock to type the randomly displayed words as quickly as you possibly can. For each accurate word that you type, a new word will appear and additional time will be added to the clock.
+Challenge yourself as you race against the clock to type the randomly displayed words as quickly as you possibly can.
+For each accurate word that you type, a new word will appear and additional time will be added to the clock.
 
-Accumulate points for each accurately typed word, but don't let the time run out or it'll be game over. Speed Typer is a fun and exciting way to practice your typing skills that will have you hooked.
+Accumulate points for each accurately typed word, but don't let the time run out, or it'll be game over. Speed Typer is
+a fun and exciting way to practice your typing skills that will have you hooked.
 
-Speed Typer generates random words from the [Random Words API](https://github.com/mcnaveen/Random-Words-API) and places it in the DOM. The application has a settings toggle that not only allows the player to toggle between difficulties, but stores difficulty setting in local storage.
+Speed Typer generates random words from a custom API and places it in the DOM. The application has a settings toggle
+that not only allows the player to toggle between difficulties, but
+stores difficulty setting in localStorage.
 
 ## User Story
 
@@ -46,7 +54,13 @@ WHEN I type in the word correctly
 THEN I am presented with a new word and additional time will be added to the clock.
 
 WHEN I have run out of time
-THEN I am shown the final score and options to play again or quit.
+THEN I am given a prompt to record my score.
+
+WHEN I have entered my name and saved my score
+THEN I am presented with the top five scores.
+
+WHEN I close the high score table
+THEN I am presented with options to play again or quit.
 
 WHEN I click on the "settings" icon
 THEN I am presented with a menu to toggle between three difficulty settings.
@@ -54,30 +68,27 @@ THEN I am presented with a menu to toggle between three difficulty settings.
 
 ## Preview of Speed Typer
 
+![Speed Typer Preview](images/SpeedTyperLandingPage.png)
+
 The following animation demonstrates the complete application functionality:
 
-<p align="center">
-  <img src="https://github.com/rh9891/SpeedTyper/blob/main/images/speedTyperFunctionality.gif">
-</p>
-
-To download the above video file of the application functionality, you may do so [here](https://github.com/rh9891/SpeedTyper/blob/main/images/speedTyperFunctionality.mp4).
+![Speed Typer Functionality]()
 
 ## Links
 
 - [Deployed Application](https://rh9891.github.io/SpeedTyper)
 
-- [Github Repository](https://github.com/rh9891/SpeedTyper)
+- [Github Repository](https://go-speed-typer-go.netlify.app/)
 
 ## Built Using
 
-Listed below are the frameworks, libraries, and guides that made building this application possible:
+Listed below are the resources and guides that made building this application possible:
 
-- [JavaScript](https://www.w3schools.com/js/default.asp)
-- [JavaScript HTML DOM](https://www.w3schools.com/js/js_htmldom.asp)
-- [Window localStorage Property](https://www.w3schools.com/jsref/prop_win_localstorage.asp)
-- [Window setInterval() Method](https://www.w3schools.com/jsref/met_win_setinterval.asp)
-- [Web APIs](https://www.w3schools.com/js/js_api_intro.asp)
-- [Traversy Media Tutorials](https://www.traversymedia.com)
+- [localStorage Property](https://www.w3schools.com/jsref/prop_win_localstorage.asp)
+- [setInterval() Method](https://www.w3schools.com/jsref/met_win_setinterval.asp)
+- [setTimeout() Method](https://www.w3schools.com/jsref/met_win_settimeout.asp)
+- [clearInterval() Method](https://www.w3schools.com/jsref/met_win_clearinterval.asp)
+- [clearTimeout() Method](https://www.w3schools.com/jsref/met_win_cleartimeout.asp)
 
 ## License
 
@@ -85,12 +96,15 @@ The MIT License (MIT)
 
 Copyright (c) 2021 Romie Hecdivert
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-## Questions
-
-If you have any questions, comments, or issues regarding this application, please do not hesitate to contact me via [Github](https://github.com/rh9891).
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- game.js
+++ game.js
@@ -1,16 +1,17 @@
 // DOM Elements.
 const word = document.getElementById("word");
 const text = document.getElementById("text");
+const toast = document.getElementById("toast");
 const scoreElement = document.getElementById("score");
 const timeElement = document.getElementById("time");
+const gameElement = document.getElementById("game-container");
 const endGameElement = document.getElementById("end-game-container");
+const highScoreEntry = document.getElementById("high-score-board-entry");
 const settingsButton = document.getElementById("settings-button");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
+const highScoreBoard = document.getElementById("high-score-board");
 const difficultySelect = document.getElementById("difficulty");
-
-// Array of words for game.
-let words = [];
 
 // Initialize the word.
 let randomWord;
@@ -23,11 +24,11 @@ let time = 15;
 
 // Sets the difficulty to value in local storage or to the default value (medium).
 let difficulty =
-  localStorage.getItem("difficulty") !== null
-    ? localStorage.getItem("difficulty")
-    : "medium";
+    localStorage.getItem("difficulty") !== null
+        ? localStorage.getItem("difficulty")
+        : "medium";
 
-// Sets the difficulty select value to its default value or to whatever is saved in local storage.
+// Sets the difficulty select value to its default value or to whatever is saved in localStorage.
 difficultySelect.value = difficulty;
 
 // On game start, calling focus method to focus on text.
@@ -36,99 +37,191 @@ text.focus();
 // Function to start the timer to count down.
 const timeInterval = setInterval(updateTime, 1000);
 
-// Function to generate a random word from the Random Words API created by MC Naveen (https://github.com/mcnaveen/Random-Words-API).
-getRandomWord = async () => {
-  const res = await fetch("https://random-words-api.vercel.app/word");
-
-  const data = await res.json();
-
-  return data[Object.keys(data)[0]].word.toLowerCase();
+// Function to get random word from API.
+const getRandomWord = async () => {
+    const res = await fetch("https://random-word-api.herokuapp.com/word");
+    const data = await res.json();
+    return data[0].toLowerCase();
 };
+
 getRandomWord();
 
 // Function to update the timer and display it on the DOM.
 function updateTime() {
-  time--;
-  timeElement.innerHTML = time + "s";
+    time--;
+    timeElement.innerHTML = time + "s";
 
-  // When the time is 5 seconds or less, it is displayed in a different color.
-  if (time < 6) {
-    timeElement.style.color = "#35caed";
-  }
+    // When the time is 5 seconds or less, it is displayed in a different color.
+    if (time < 6) {
+        timeElement.style.color = "#35caed";
+    }
 
-  // Disables the difficulty selection once the game has started.
-  if (time < 15) {
-    document.getElementById("difficulty").disabled;
-  }
+    // Disables the difficulty selection once the game has started.
+    if (time < 15) {
+        document.getElementById("difficulty").disabled;
+    }
 
-  // Disables the difficulty selection once the game is over.
-  if (time === 0) {
-    document.getElementById("difficulty").disabled;
-    clearInterval(timeInterval);
+    // Disables the difficulty selection once the game is over.
+    if (time === 0) {
+        document.getElementById("difficulty").disabled;
+        clearInterval(timeInterval);
 
-    // Function to end game.
-    gameOver();
-  }
+        // Function to end game.
+        gameOver();
+    }
 }
-
-// Function to end game and display the end screen on DOM.
-gameOver = () => {
-  endGameElement.innerHTML = `
-<h1>Sorry! Time has run out.</h1>
-<p class="final-score-message">Your final score is ${score}.</p>
-<button onClick="window.location.reload()" class="button play-again-button">Play Again</button>
-<a class="quit-link" href="index.html"><button class="button quit-button">Quit</button></a>
-`;
-
-  endGameElement.style.display = "flex";
-};
 
 // Function to display random word onto DOM.
 addWordToDOM = async () => {
-  randomWord = await getRandomWord();
-  word.innerHTML = randomWord;
+    randomWord = await getRandomWord();
+    word.innerHTML = randomWord;
 };
 
 // Function to update the score and display it on DOM.
 updateScore = () => {
-  score++;
-  scoreElement.innerHTML = score;
+    score++;
+    scoreElement.innerHTML = score;
 };
 
 addWordToDOM();
 
-// Adds event listener to the input field, so that the user-inputted word or phrase is targeted. If the displayed word and the input phrase are the same, then a new word is displayed on the DOM and the input field is cleared.
+// Event listener when typing the word.
 text.addEventListener("input", (event) => {
-  const insertedText = event.target.value;
+    const insertedText = event.target.value;
 
-  if (insertedText === randomWord) {
-    addWordToDOM();
-    updateScore();
+    if (insertedText === randomWord) {
+        addWordToDOM();
+        updateScore();
 
-    // Clears the input field.
-    event.target.value = "";
+        // Clears the input field.
+        event.target.value = "";
 
-    // Appends additional seconds to time (dependent on difficulty level) and updates the time.
-    if (difficulty === "hard") {
-      time += 2;
-    } else if (difficulty === "normal") {
-      time += 3;
-    } else if (difficulty === "easy") {
-      time += 5;
+        // Appends additional seconds to time (dependent on difficulty level) and updates the time.
+        if (difficulty === "hard") {
+            time += 2;
+        } else if (difficulty === "normal") {
+            time += 3;
+        } else if (difficulty === "easy") {
+            time += 5;
+        }
+
+        updateTime();
     }
-
-    updateTime();
-  }
 });
 
 // Event listener when clicking the settings icon.
 settingsButton.addEventListener("click", () =>
-  settings.classList.toggle("hide")
+    settings.classList.toggle("hide")
 );
 
 // Event listener when selecting the settings difficulty.
 settingsForm.addEventListener("change", (event) => {
-  difficulty = event.target.value;
+    difficulty = event.target.value;
 
-  localStorage.setItem("difficulty", difficulty);
+    localStorage.setItem("difficulty", difficulty);
 });
+
+const highScoreKey = "highScores";
+const maxHighScores = 5;
+
+// Retrieve high scores from localStorage.
+function getHighScores() {
+    const storedScores = localStorage.getItem(highScoreKey);
+    return storedScores ? JSON.parse(storedScores) : [];
+}
+
+// Save high scores to localStorage.
+function saveHighScores(newScore, playerName) {
+    const highScores = getHighScores();
+
+    // Add the new score and sort.
+    highScores.push({score: newScore, name: playerName});
+    highScores.sort((a, b) => b.score - a.score);
+
+    // Keep only the top `maxHighScores` scores.
+    if (highScores.length > maxHighScores) {
+        highScores.length = maxHighScores;
+    }
+
+    localStorage.setItem(highScoreKey, JSON.stringify(highScores));
+}
+
+// Displays high scores in the board.
+function showHighScores() {
+    const highScores = getHighScores();
+
+    // Generate the HTML for the high score board.
+    highScoreBoard.innerHTML = `
+        <h1>High Scores</h1>
+        <ul id="high-score-list">
+            ${highScores.map((score) => `<li>${score.name}: ${score.score}</li>`).join("")}
+        </ul>
+        <button class="button close-score-button">Close</button>
+    `;
+
+    // Show the high-score board.
+    highScoreBoard.style.display = "flex";
+
+    // Add event listener for the "Close" button.
+    document.querySelector(".close-score-button").addEventListener("click", () => {
+        // Hide the high-score board
+        highScoreBoard.style.display = "none";
+
+        // Show the end-game container
+        endGameElement.style.display = "flex";
+    });
+}
+
+// Function to show a toast message.
+function showToast(message) {
+    toast.textContent = message;
+    toast.classList.add("show");
+
+    // Hide the toast after 3 seconds
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 3000);
+}
+
+gameOver = () => {
+    // Update the end game container with a message and options.
+    endGameElement.innerHTML = `
+        <h1>Game Over</h1>
+        <p>Think you can beat your high score? Give it another shot!</p>
+        <div class="button-container">
+            <a class="restart-link"><button onClick="window.location.reload()" class="button">Play Again</button></a>
+            <a class="quit-link" href="index.html"><button class="button">Quit</button></a>
+        </div>
+    `;
+
+    // Hide the game container.
+    gameElement.style.display = "none";
+
+    // Add high-score entry form after hiding the end game container.
+    highScoreEntry.innerHTML = `
+        <h1>Sorry! Time has run out.</h1>
+        <p>Your final score is ${score}.</p>
+        <p>Enter your name to save your score:</p>
+        <input type="text" id="player-name" placeholder="Enter First Name" class="input-field" />
+        <button id="save-score-button" class="button">Save Score</button>
+    `;
+    highScoreEntry.style.display = "flex";
+
+    // Hide the end-game container.
+    endGameElement.style.display = "none";
+
+    // Add event listener to handle saving the score.
+    document.getElementById("save-score-button").addEventListener("click", () => {
+        const playerName = document.getElementById("player-name").value.trim();
+        if (playerName) {
+            saveHighScores(score, playerName);
+
+            // Hide the name entry container and show the high score board.
+            highScoreEntry.style.display = "none";
+            showHighScores();
+        } else {
+            // Show a toast notification if the input is invalid.
+            showToast("Please enter a valid name!");
+        }
+    });
+};
